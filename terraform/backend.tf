61ae646bde4d20184b404b063d0475b3ac230ef1
Gaurav Rajendra Girase <78299809+GauravGirase@users.noreply.github.com>
2026-02-15 18:34:16 +0530

    Delete terraform directory

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "devsecops-terraform-state-2026"
-    key = "dev/terraform.tfstate"
-    region = "ap-south-1"
-    use_lockfile = true
-    encrypt = true
-  }
-}
--- terraform/output.tf
+++
@@ -1,6 +0,0 @@
-output "public_ip" {
-  value = aws_instance.server.public_ip
-}
-output "ssh_command" {
-  value = "ssh -i 'new-keypair.pem' ubuntu@${aws_instance.server.public_dns}"
-}
